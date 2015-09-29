f9e285fa9c297b458bad73b4f420756bb30ac052
Miguel Molero <miguel.molero@gmail.com>
2015-09-29 13:38:40 +0200

    Update

--- Readme.md
+++ Readme.md
@@ -1,9 +1,9 @@
 
-##Poisson Surface Reconstruction Python Wrapper 
+##Poisson Surface Reconstruction Python Binding 
 
 v6.13 
 
-Python Wrapper of Poisson Reconstruction by  [Michael Kazhdan] (http://www.cs.jhu.edu/~misha/Code/PoissonRecon/Version6.13/)
+Python Binding of Poisson Reconstruction by  [Michael Kazhdan] (http://www.cs.jhu.edu/~misha/Code/PoissonRecon/Version6.13/)
 for C++ GNU Compilation [PoissonRecon_v6_13](https://github.com/mmolero/PoissonRecon_v6_13) based on the MATLAB MEX-File bindings by [Daeyun Shin](https://github.com/daeyun/poisson-surface-reconstruction)
 
 
--- setup.py
+++ setup.py
@@ -4,11 +4,10 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy
 
-sources = ["pypoisson.pyx"]
-path = "PoissonRecon_v6_13/src/"
+sources = ["src/pypoisson.pyx"]
+path = "src/PoissonRecon_v6_13/src/"
 files = [os.path.join(path,x) for x in os.listdir(path) if x.endswith(".cpp")]
 sources += files
-
 
 
 exts = [Extension("pypoisson", sources,
@@ -16,22 +15,14 @@ exts = [Extension("pypoisson", sources,
         extra_compile_args = ["-w","-fopenmp"],
         extra_link_args=["-fopenmp"]
         )]
+
 setup(
     name='pypoisson',
     version='0.10',
-    description='Poisson Surface Reconstruction Python Wrapper',
+    description='Poisson Surface Reconstruction Python Binding',
     author='Miguel Molero-Armenta',
     author_email='miguel.molero@gmail.com',
     url='https://github.com/mmolero/pypoisson',
     cmdclass = {'build_ext': build_ext},
     ext_modules = exts, include_dirs = [numpy.get_include()]
-
 )
-
-
-
-"""
-python setup.py build_ext --inplace --compiler=mingw32 -DMS_WIN64
-
-
-"""
